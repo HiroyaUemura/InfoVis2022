1192296f43cab346e0b1852b27ebae11be860545
HiroyaUemura <227x001x@stu.kobe-u.ac.jp>
2022-05-11 15:16:07 +0900

    Initial commit

--- W08/w08_task1_main.js
+++ W08/w08_task1_main.js
@@ -44,7 +44,7 @@ class BarChart {
             .domain([0, d3.max(self.data, d => d.value)])
             .range([0, self.inner_width]);
 
-        self.yscale = d3.scaleLinear()
+        self.yscale = d3.scaleBand()
             .domain(self.data.map(d => d.label))
             .range([0, self.inner_height])
             .paddingInner(0.1);
@@ -54,18 +54,20 @@ class BarChart {
             .tickSizeOuter(0);
 
         self.xaxis_group = self.chart.append('g')
-            .attr('transform', `translate(0, ${self.inner_height})`)
-            .call( xaxis );
+            .attr('transform', `translate(0, ${self.inner_height})`);
+            //.call( xaxis );
 
         self.yaxis = d3.axisLeft( self.yscale )
             .tickSizeOuter(0);
 
-        self.yaxis_group = self.chart.append('g')
-            .call( yaxis );
+        self.yaxis_group = self.chart.append('g');
+            //.call( yaxis );
     }
 
     update(){
       let self = this;
+      //self.xscale.domain([0, d3.max(self.data, d => d.value)]);
+      //self.yscale.domain(self.data.map(d => d.label);
       self.render();
     }
 
@@ -77,13 +79,13 @@ class BarChart {
           .enter()
           .append("rect")
           .attr("x", 0)
-          .attr("y", d => yscale(d.label))
-          .attr("width", d => xscale(d.value))
-          .attr("height", yscale.bandwidth());
+          .attr("y", d => self.yscale(d.label))
+          .attr("width", d => self.xscale(d.value))
+          .attr("height", self.yscale.bandwidth());
 
-          self.xaxis_group
-              .call( self.xaxis );
-          self.yaxis_group
-              .call( self.yaxis );
+      self.xaxis_group
+          .call( self.xaxis );
+      self.yaxis_group
+          .call( self.yaxis );
     }
 }
