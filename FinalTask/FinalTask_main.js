455c5c54da7351c2b3783723f4f9ea063a168782
HiroyaUemura <227x001x@stu.kobe-u.ac.jp>
2022-06-05 22:59:38 +0900

    Initial commit

--- FinalTask/FinalTask_main.js
+++ FinalTask/FinalTask_main.js
@@ -4,9 +4,9 @@ d3.csv("https://hiroyauemura.github.io/InfoVis2022/FinalTask/data.csv")
 
         var config = {
             parent: '#drawing_region',
-            width: 256,
-            height: 128,
-            margin: {top:10, right:10, bottom:20, left:60}
+            width: 512,
+            height: 512,
+            margin: {top:10, right:10, bottom:30, left:60}
         };
 
         const barchart = new BarChart( config, data );
@@ -20,9 +20,9 @@ class BarChart {
     constructor( config, data ) {
         this.config = {
             parent: config.parent,
-            width: config.width || 256,
-            height: config.height || 128,
-            margin: config.margin || {top:10, right:10, bottom:20, left:60}
+            width: config.width || 512,
+            height: config.height || 512,
+            margin: config.margin || {top:10, right:10, bottom:30, left:60}
         }
         this.data = data;
         this.init();
@@ -34,15 +34,18 @@ class BarChart {
             .attr('width', self.config.width)
             .attr('height', self.config.height);
 
-        self.series = d3.stack().keys(["paper","digital"])(data);
+        self.chart = self.svg.append('g')
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+
+        self.stackGen = d3.stack().keys(["paper", "digital"]);
+
+        self.series = self.stackGen(self.data);
+    //    self.series = d3.stack().keys(["paper", "digital"])(self.data);
 
         self.color = d3.scaleOrdinal()
             .domain(self.series.map(d => d.key))
             .range(d3.schemeCategory10.slice(0, self.series.length))
             .unknown("#ccc");
-
-        self.chart = self.svg.append('g')
-            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
 
         self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
@@ -56,48 +59,51 @@ class BarChart {
             .domain([0, d3.max(self.series, d => d3.max(d, d => d[1]))])
             .range([self.inner_height, 0]);
 
-        self.xaxis = d3.axisBottom( self.xscale )
-            .ticks(5)
-            .tickSizeOuter(0);
+        self.xaxis = self.chart.append('g')
+            .attr("transform", `translate(0, ${self.inner_height})`)
+            .call(d3.axisBottom(self.xscale)
+            .tickSizeOuter(0)
+            .tickFormat(self.year))
+            .call(g => g.selectAll(".domain").remove());
 
-        self.xaxis_group = self.chart.append('g')
-            .attr('transform', `translate(0, ${self.inner_height})`)
-            .call( self.xaxis );
-
-        self.yaxis = d3.axisLeft( self.yscale )
-            .tickSizeOuter(0);
-
-        self.yaxis_group = self.chart.append('g')
-            .call( self.yaxis );
+        self.yaxis = self.chart.append('g')
+            .call(d3.axisLeft(self.yscale)
+            .ticks(null, "s"))
+            .call(g => g.selectAll(".domain").remove());
     }
 
     update(){
       let self = this;
-
-      self.xscale.domain(self.data.map(d => d.year));
-      self.yscale.domain([0, d3.max(self.series, d => d3.max(d, d => d[1]))]);
-
       self.render();
     }
 
     render(){
       let self = this;
 
+//      console.log(self.data);
+//      console.log(self.series);
+
       self.chart.selectAll("g")
           .data(self.series)
-          .join("g")
-            .attr("fill", d => color(d.key))
+          .enter().append("g")
+            .attr("fill", function(d) { return self.color(d.key); } )
           .selectAll("rect")
-          .data(d => d)
+          .data(function(d) { return d; })
+      //    .enter().append("rect")
           .join("rect")
-            .attr("x", (d, i) => self.xscale(d.data.year))
-            .attr("y", d => self.yscale(d[1]))
-            .attr("height", d => self.yscale(d[0]) - self.yscale(d[1]))
-            .attr("width", self.xscale.bandwidth());
+            .attr("x", function(d) { return self.xscale(d.data.year); } )
+  //          .attr("y", 0)
+            .attr("y", function(d) { return self.yscale(d[0]); } )
+        //    .attr("fill", d => self.color(d.key))
+            .attr("width", self.xscale.bandwidth())
+            .attr("height", function(d) { return self.yscale(d[1]) - self.yscale(d[0]); } );
 
-      self.xaxis_group
-          .call( self.xaxis );
-      self.yaxis_group
-          .call( self.yaxis );
+
+
+
+  //    self.xaxis
+//          .call( self.xaxis );
+//      self.yaxis
+  //        .call( self.yaxis );
     }
 }
